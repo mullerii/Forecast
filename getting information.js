045957498be4137127fa2553e4563dc60708c1af
Antonia Müller <161516204+mullerii@users.noreply.github.com>
2024-02-27 21:52:59 +0200

    Create getting information.js

---
+++ getting information.js
@@ -0,0 +1,15 @@
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// sites that provide information
+		define([], forecast);
+
+
+  	var vertexShaderCode =
+	"attribute vec2 position;\n" +
+	"varying vec2 pos;\n" +
+
+
+
+const objectsContents = [];
+const pathMap = {};
+const listOfNotFound = [];
