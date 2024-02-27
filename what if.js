e823ea5842484e26818f69b745d5c22798a4002b
Antonia Müller <161516204+mullerii@users.noreply.github.com>
2024-02-27 21:55:27 +0200

    Create what if.js

---
+++ what if.js
@@ -0,0 +1,16 @@
+{
+  "main": "index.js",
+  "dependencies": {}
+
+if ( badweather ) {
+
+	console.log( 'bad weather and raining.' );
+	process.exit( - 1 );
+
+}
+
+	const originalFileName = fileName;
+
+	let prefix = '';
+	let objectContent = null;
+	for ( let attempt = 0; attempt < 2; attempt ++ ) {
